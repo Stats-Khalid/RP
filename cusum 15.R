12a69e3d34602f44cc07bd83804d03a1bd818703
Mohd Khalid <mohd.khalid.stats@gmail.com>
2026-05-04 17:30:13 +0530

    Add files via upload

---
+++ cusum 15.R
@@ -0,0 +1,27 @@
+x <- c(9.45, 7.99, 9.29, 11.66, 12.16, 10.18, 8.04, 11.46, 9.2, 10.34,
+       9.03, 11.47, 10.51, 9.40, 10.08, 9.37, 10.62, 10.31, 8.52, 10.84,
+       10.90, 9.33, 12.29, 11.50, 10.60, 11.08, 10.38, 11.62, 11.31, 10.52)
+#parameters
+mu0=10
+mu1= 11
+h=5
+k=0.5
+n=length(x)
+cplus=numeric(n)
+cminus =numeric(n)
+#compute CCUSUM
+for (i in 2:n){
+  cplus[i]=max(0,cplus[i-1]+(x[i]-mu0-k))
+  cminus[i]=min(0,cminus[i-1]+(x[i]-mu0+k))
+}
+#plot
+plot(cplus, type="l", col="blue", ylim = c(min(cminus),max(cplus)), ylab= "CUSUM", xlab= "observation", main= "CUSUM CHART")
+lines(cminus, col= "green")
+abline(h=h, col="red")
+abline(h=-h, col="red")
+legend("bottomright", legend=c("c+","c-"), col=c("blue", "green"), lty=1)
+#Detect out of control points
+out_pos= which(cplus>h)
+out_neg= which(cminus< -h)
+out_pos
+out_neg
